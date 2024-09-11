dbd4437d8be6ece566e65f4cf457722137be3a7d
Emeka Anyaegbunam <emekaanyaegbunam@Emekas-MacBook-Pro.local>
2024-09-11 12:12:07 +0100

    profile

--- components/NotNeeded/useIsDashboard.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { usePathname } from "next/navigation";
-
-export function useIsDashboard() {
-    const pathname = usePathname();
-    return pathname.startsWith('/dashboard');
-}
